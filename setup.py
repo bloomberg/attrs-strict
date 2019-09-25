aaa4a667b740fe6ea98cd66b7e206646bfea3f2e
Erik-Cristian S <eseulean@bloomberg.net>
2019-09-25 13:46:02 +0100

    Add publishing (#5)
    * Add publishing
    
    Signed-off-by: Erik <eseulean@bloomberg.net>
    
    * Automatically generate version number based on commit
    
    Signed-off-by: Erik <eseulean@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
-from setuptools import setup
+import textwrap
 
-try:
-    from attrs_strict._version import __version__
-except ImportError:
-    _version_globals = {}
-    with open("attrs_strict/_version.py") as fp:
-        exec(fp.read(), _version_globals)
-    __version__ = _version_globals["__version__"]
+from setuptools import setup
 
 setup(
     name="attrs-strict",
-    version=__version__,
     description="Runtime validators for attrs",
     long_description="file: README.md",
     long_description_content_type="text/markdown",
@@ -21,6 +14,30 @@ setup(
     install_requires=["attrs"],
     tests_require=["mock", "pytest"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    use_scm_version={
+        "write_to": "attrs_strict/_version.py",
+        "write_to_template": textwrap.dedent(
+            """
+            __version__ = {version!r}
+
+            # -----------------------------------------------------------------------------
+            # Copyright 2019 Bloomberg Finance L.P.
+            #
+            # Licensed under the Apache License, Version 2.0 (the "License");
+            # you may not use this file except in compliance with the License.
+            # You may obtain a copy of the License at
+            #
+            #     http://www.apache.org/licenses/LICENSE-2.0
+            #
+            # Unless required by applicable law or agreed to in writing, software
+            # distributed under the License is distributed on an "AS IS" BASIS,
+            # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+            # See the License for the specific language governing permissions and
+            # limitations under the License.
+            # ----------------------------- END-OF-FILE -----------------------------------
+            """
+        ).lstrip(),
+    },
 )
 
 # -----------------------------------------------------------------------------
